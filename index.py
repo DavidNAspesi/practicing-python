643dbabc929d27eaa4a96ceaacb5646b2f321457
David Aspesi <davidnaspesi@gmail.com>
2018-08-13 13:46:46 -0600

    working with tuples

--- index.py
+++ index.py
@@ -8,6 +8,10 @@ b = "ABRACADABRA he yelled"
 
 c = {"key1":"name","key2":"age"}
 
+d = {"k1":1,"k2":[0,1,2],"k3":{"anotherKey":100}}
+
+e = (1,"two",4.5)
+
 
 print(a[::-1])
 print(b.split())
@@ -18,3 +22,7 @@ print("this is a string {1} {0}".format("also inserted", "inserted"))
 print(f'this is a new string {b}')
 
 print(c["key1"])
+
+print(d["k3"]["anotherKey"])
+
+print(e.count("two"))
