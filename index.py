483b594b3fd793e13c54a115b8de4bd497447cfe
David Aspesi <davidnaspesi@gmail.com>
2018-08-15 11:34:50 -0600

    working with comparison operators

--- index.py
+++ index.py
@@ -1,32 +1,46 @@
-# print("hello world")
-# print(2 ** 3)
+# # print("hello world")
+# # print(2 ** 3)
 
-a = 5
-a = "Hello World"
+# a = 5
+# a = "Hello World"
 
-b = "ABRACADABRA he yelled"
+# b = "ABRACADABRA he yelled"
 
-c = {"key1":"name","key2":"age"}
+# c = {"key1":"name","key2":"age"}
 
-d = {"k1":1,"k2":[0,1,2],"k3":{"anotherKey":100}}
+# d = {"k1":1,"k2":[0,1,2],"k3":{"anotherKey":100}}
 
-e = (1,"two",4.5)
+# e = (1,"two",4.5)
 
-myfile = open("myfile.txt")
+# with open("myfile.txt") as myfile:
+#     contents = myfile.readlines()
 
-# print(a[::-1])
-# print(b.split())
+# with open("abdcd.txt", mode="w") as f:
+#     f.write("Just created this file!")
+
+# with open("abdcd.txt", mode="r") as l:
+#     stuff = l.read()
+
+# # print(a[::-1])
+# # print(b.split())
 
 
-# print("this is a string {1} {0}".format("also inserted", "inserted"))
+# # print("this is a string {1} {0}".format("also inserted", "inserted"))
 
-# print(f'this is a new string {b}')
+# # print(f'this is a new string {b}')
 
-# print(c["key1"])
+# # print(c["key1"])
 
-# print(d["k3"]["anotherKey"])
+# # print(d["k3"]["anotherKey"])
 
-# print(e.count("two"))
+# # print(e.count("two"))
 
-# print(1 > 2)
-print(myfile.read())
+# # print(1 > 2)
+
+# print(contents)
+# print(stuff)
+
+print(2 == 2)
+print("2" == 2)
+print (2.1 > 2)
+print(3 != 2)
